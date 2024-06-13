e41e062e905ace8a0eb37be205ed15ae0188a5ae
root <root@aio.example.com>
2024-06-13 14:29:08 +0900

    hehehehe

---
+++ Containerfile
@@ -0,0 +1,2 @@
+FROM quay.io/startx/php:latest
+COPY ip-v3.php /app
---
+++ ip-v3.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+$hostname_server = gethostname();
+
+echo "this PHP APP version is v3";
+echo $ip_server;
+echo $hostname_server;
+?>
